a59176f59d473a42f7fed59cfdbc62e6e751d295
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-05-07 13:11:07 +0200

    ..

--- src/chatterbox/ChatClient.java
+++ src/chatterbox/ChatClient.java
@@ -5,7 +5,7 @@ public class ChatClient {
     public static void main(String[] args) {
         System.out.println("Hallo!");
 
-        
+
 
     }
 }
---
+++ src/linkedlist/Node.java
@@ -0,0 +1,11 @@
+package linkedlist;
+
+public class Node {
+    public Node(Object data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+    Object data;
+    Node next;
+
+}
